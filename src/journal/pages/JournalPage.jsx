427575a141c11bfdac591ca841a91e5ebb50f3ae
aguslopez9 <agustinlopezv99@gmail.com>
2023-09-04 18:39:54 -0300

    journal layout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,47 @@
 
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 
 export const JournalPage = () => {
   return (
-    <>
-    <Typography variant='h1'>JournalPage</Typography>
+    <JournalLayout>
+    <Typography>Saliste ya mil veces
+de la pista a respirar
+a reclutar, bien maquillado
+y ocultando tu lunar.
+Un día el bote volcó
+y el premio a pique se fue.
+Todos te daban por muerto
+y vos allí en mi remolque
+sin luz, como un polizón.
+Mirá qué tipo espeso
+sumiso como un guiso más
+un vago de mil caravanas
+a punto de quedar a pie.
+Fundiendo plomo lográs
+chorros de oro cochino
+en besos de lo más desnudos
+pero el café con tu suerte
+se enfría en mi mesa fría.
+Salando las heridas
+jodiste a todo Cristo y más...
+a boluditos de la luna
+y tipas porno-nazi look.
+Tu lengua se derrite
+en modas de la rabia de hoy
+cuando enfermás con tanta gana
+cerrás las filas del dolor.
+Apuntamos a tu nariz
+hundimos tus pómulos
+y vos resplandecías
+no te quedó sueño por vengar
+y ya no esper s que te jueguen limpio
+nunca más.
+</Typography>
 
-    </>
+{/* Nothing selected */}
+{/* note view */}
+    </JournalLayout>
   )
 }
