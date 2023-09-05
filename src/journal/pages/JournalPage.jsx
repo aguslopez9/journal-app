a99a889d4d56171a4527ab2e88d632e3f8e63584
aguslopez9 <agustinlopezv99@gmail.com>
2023-09-05 19:03:54 -0300

    lista la carpinteria

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,8 @@
 
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedView } from "../views"
+import { NoteView, NothingSelectedView } from "../views"
+import { AddOutlined } from "@mui/icons-material"
 
 
 export const JournalPage = () => {
@@ -40,9 +41,24 @@ no te quedó sueño por vengar
 y ya no esper s que te jueguen limpio
 nunca más.
 </Typography> */}
+
 <NothingSelectedView/>
 
-{/* note view */}
+{/* <NoteView/> */}
+    
+    <IconButton
+      size="large"
+      sx={{
+        color: 'white',
+        backgroundColor: 'error.main',
+        ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+        position: 'fixed',
+        right: 50,
+        bottom: 50
+      }}
+      >
+        <AddOutlined sx={{fontSize: 30}}/>
+    </IconButton>
     </JournalLayout>
   )
 }
